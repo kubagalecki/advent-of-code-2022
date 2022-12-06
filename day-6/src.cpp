6a16aa79800cdfec10399696254f026028218d1e
Jakub Gałecki <jgalecki@meil.pw.edu.pl>
2022-12-06 10:14:10 +0100

    day 6

---
+++ day-6/src.cpp
@@ -0,0 +1,35 @@
+#include "../common/common.hpp"
+
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <span>
+
+bool isUnique(std::span<const char> input) {
+  std::array<bool, 256> map{};
+  return std::ranges::none_of(
+      input, [&](char c) { return std::exchange(map[c], true); });
+}
+
+size_t firstUniqueAfter(std::string_view input, size_t window_size) {
+  const auto span = std::span{input};
+  for (size_t i = 0; i < span.size() - window_size; ++i)
+    if (isUnique(span.subspan(i, window_size)))
+      return i + window_size;
+  return 0;
+}
+
+void part1() {
+  const auto [alloc, data] = getStdinView();
+  std::cout << firstUniqueAfter(data, 4) << '\n';
+}
+
+void part2() {
+  const auto [alloc, data] = getStdinView();
+  std::cout << firstUniqueAfter(data, 14) << '\n';
+}
+
+int main() {
+  part1();
+  part2();
+}
